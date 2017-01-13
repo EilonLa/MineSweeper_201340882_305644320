e9628c9d74cd53783592dd1728ee47490b22ce21
אילון <ilonlaor123>
2017-01-13 13:43:10 +0200

    הגשה 13.1

--- app/src/main/java/activities/EndActivity.java
+++ app/src/main/java/activities/EndActivity.java
@@ -24,6 +24,7 @@ public class EndActivity extends FragmentActivity {
     private int score;
     private Location lastLocation;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
